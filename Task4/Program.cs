d23b65e46de4b58500e91bcfe4909f0a7ce0b41e
Max <seven.85@list.ru>
2024-03-08 16:03:43 +0500

    add Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,10 @@
+﻿string str = "Hello my world!";
+
+string reversedStr = "";
+string[] words = str.Split(' ');
+for (int i = words.Length - 1; i >= 0; i--)
+{
+	reversedStr += words[i] + " ";
+}
+
+Console.WriteLine(reversedStr);
