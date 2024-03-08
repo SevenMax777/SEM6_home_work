11e9df9d4b8809e67600c7b662658224b6dd7957
Max <seven.85@list.ru>
2024-03-08 15:19:00 +0500

    add Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿char[,] array = new char[3, 3] {
+	{ '1', '2', '3'},
+	{ '4', '5', '6'},
+	{ '7', '8', '9'}
+};
+
+string str = "";
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+	for (int j = 0; j < array.GetLength(1); j++)
+	{
+		str += array[i, j];
+	}
+}
+
+Console.WriteLine(str);
