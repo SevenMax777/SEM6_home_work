158caeae9e5bdfcdf025ce81de95017be7b6317d
Max <seven.85@list.ru>
2024-03-08 15:44:32 +0500

    add Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,30 @@
+﻿string str = "aBcD1ef!";
+
+bool IsPalindrome(string str)
+{
+	char[] chars = str.ToCharArray();
+	int start = 0, end = chars.Length - 1;
+	while (start < end)
+	{
+   	if (chars[start] != chars[end])
+   	{
+   		return false;
+   	}
+
+   start++;
+   end--;
+	}
+
+	return true;
+}
+
+bool isPalindrome = IsPalindrome(str);
+if (isPalindrome)
+{
+	Console.WriteLine("Строка является палиндромом");
+}
+else
+{
+	Console.WriteLine("Строка не является палиндромом");
+}
+
